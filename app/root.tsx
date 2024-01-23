27ef6ce78be1a9d2bb02ee2e4987a4c6898c1434
renoTahoeWebDev <info@devwebmike.com>
2024-01-22 22:30:56 -0800

    staging init for netlify auto deploys

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -6,13 +6,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from "@remix-run/react"
 
-export const meta: MetaFunction = () => [{
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-}];
+export const meta: MetaFunction = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+  },
+]
 
 export default function App() {
   return (
@@ -22,11 +24,12 @@ export default function App() {
         <Links />
       </head>
       <body>
+        staging
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
